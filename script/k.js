11130e933d2ace2f8b5754e78c954c21d41e42c7
Khaledul40 <khaledul40@gmail.com>
2024-02-20 03:05:57 +0600

    added js

---
+++ script/k.js
@@ -0,0 +1,51 @@
+const allBtn = document.getElementsByClassName("ticket");
+let count = 0;
+let deduct = 8;
+let clas = "Economy";
+
+
+
+for (const btn of allBtn) {
+    btn.addEventListener("click", function (e) {
+        count = count + 1;
+        deduct = deduct - 1;
+         clas = "Economy" 
+
+         const selectTicket = e.target.innerText;
+         const ticketClass = e .target.value = clas;
+         const priceTicket = e.target.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.childNodes[7].childNodes[1].childNodes[5].childNodes[9].childNodes[3].childNodes[0].innerText;
+
+         const selectInfo = document.getElementById("seat-info");
+ 
+         const li = document.createElement("li");
+ 
+         const p = document.createElement("p");
+         p.innerText = selectTicket;
+         const p2 = document.createElement("p");
+         p2.innerText = ticketClass;
+         const p3 = document.createElement("p");
+         p3.innerText = priceTicket;
+         
+ 
+         li.appendChild(p);
+         li.appendChild(p2);
+         li.appendChild(p3);
+        
+         selectInfo.appendChild(li);
+
+
+         
+       
+        
+       setInnerText("ttl-ticket", count) 
+       setInnerText("rest-ticket", deduct)
+    });
+}
+
+
+function setInnerText(id, value) {
+    document.getElementById(id).innerText = value;
+}
+
+
+
---
+++ script/utility.js
@@ -0,0 +1,15 @@
+// const priceTicket = e.target.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.childNodes[7].childNodes[1].childNodes[5].childNodes[9].childNodes[3].childNodes[0].innerText;
+ 
+//          
+ 
+//          const p = document.createElement("p");
+//          p.innerText = selectTicket;
+//          // const p2 = document.createElement("p");
+//          // p2.innerText = ticketClass;
+//          const p3 = document.createElement("p");
+//          p3.innerText = priceTicket;
+ 
+//          p.appendChild(p);
+//          // p.appendChild(p2);
+//          p.appendChild(p3);
+//          selectInfo.appendChild(p);
